314e3bfc3c343bbc6acaf060fc5c9d5d40014e33
saravanan <saravnsos@gmail.com>
2021-06-11 12:42:44 +0530

    'EIGS-1.0'

---
+++ src/main/java/com/eigs/rest/EigsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.eigs.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EigsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EigsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eigs/rest/EigsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eigs.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EigsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
